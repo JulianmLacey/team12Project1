9df3a12611d0da3e91a2187f258bbc9812f4876d
JulianmLacey <117090683+JulianmLacey@users.noreply.github.com>
2023-07-18 01:56:26 -0700

    Final js with updated cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,89 +1,162 @@
-/*
+var JobElementInput = $("#job_input");
+var cityElementInput = $("#city_input");
+var bathroomElementInput = $("#bathroom_input");
+var bedRoomElementInput = $("#bedroom_input");
 
-//-----------------Get Job Options-------------------
-var url = "https://jooble.org/api/";
-var key = "66db19d3-8777-40aa-a3c1-d214c3c4b8ea";
-var SearchKeywords = 'Software Engineer';
-var searchJobLocation = 'Seattle';
-var params = "{ keywords: "+SearchKeywords +", location: "+searchJobLocation;
 
-function getJobs(){
-//create xmlHttpRequest object
-var http = new XMLHttpRequest();
-//open connection. true - asynchronous, false - synchronous
-http.open("POST", url + key, true);
 
-//Send the proper header information
-http.setRequestHeader("Content-type", "application/json");
-	
-//Callback when the state changes
-http.onreadystatechange = function() {
-	if(http.readyState == 4 && http.status == 200) {
-		alert(http.responseText);
-    console.log(JSON.parse(http.responseText));
-	}
+
+
+$("#searchButton").on("click", runSearch);
+
+//search entry point
+function runSearch() {
+    var JobElementInputValue = JobElementInput.val();
+    var cityElementInputValue = cityElementInput.val();
+    var bathroomElementInputValue = bathroomElementInput.val();
+    var bedRoomElementInputValue = bedRoomElementInput.val();
+
+
+
+  console.log("searching");
+  //getHousing();
+  //getJobs();
+  //filterData();
+    console.log(JobElementInput.val());
+    console.log(cityElementInput.val());
+    console.log(bathroomElementInput.val());
+    console.log(bedRoomElementInput.val());
+
+
+    //check for valid input
+    if(JobElementInputValue == ""){
+        //alert bad jab input
+        return;
+    }
+    if(cityElementInputValue == ""){
+        //alert bad location input
+        return;
+    }
+    if(bathroomElementInputValue == ""){
+        
+    }
+    if(bedRoomElementInputValue == ""){
+        
+    }
+
+    //get the job input to be replaced with filterData(); but just saved to local storage to save calls
+    var jobInfo = JSON.parse(localStorage.getItem("JobSearch"));
+    var houseInfo = JSON.parse(localStorage.getItem("houseSearch"));
+    console.log(jobInfo.totalCount);
+    console.log(houseInfo.length);
+
+
+    var pagelength = (jobInfo.totalCount>houseInfo.length)?houseInfo.length:jobInfo.totalCount;
+
+    console.log(pagelength);
+    for(var i = 0; i<pagelength;i++){
+        renderCard(jobInfo.jobs[i].company, houseInfo[i].formattedAddress);
+    }
+
+
 }
-//Send request to the server
-http.send(params);
-console.log("Got Jobs")
-}
-
-getJobs();
-
-*/
 
 
-var container = $("#card-container");
-function renderCard(){
-
-var newCard = $("<div>");
-
-newCard.attr("class", "card");
-
-  var face1 = $("<div>").attr("class", "face face1").appendTo(newCard);
-  var faceContent1 = $("<div>").attr("class", "content").appendTo(face1);
-  $("<h3>").text("JOB TITLE").appendTo(faceContent1);
-
-  var face2 = $("<div>").attr("class", "face face2").appendTo(newCard);
-  var faceContent2 = $("<div>").attr("class", "content").appendTo(face2);
-  $("<p>").text("JOB STUFFF").appendTo(faceContent2);
+//Temp variables for getHousing Function...to be removed later*
+var Qcity = "Austin",
+Qstate = "TX";
+var Bed_rooms = 2,
+bath_rooms = 2,
+limit = 10;
 
 
-  newCard.appendTo(container);
-
-}
-
-renderCard();
-renderCard();
-renderCard();
-renderCard();
-renderCard();
+//may need to add state param*
+function getHousing(Qcity, Bed_rooms, bath_rooms) {
+  //Variable to put into params
 
 
 
-//-----------------Get Housing Options-------------------
-var Qcity = 'Austin', Qstate = 'TX';
-var Bed_rooms = 2, bath_rooms =2, limit = 10;
-
-function getHousing(){
+  //------------------------------
   const settings = {
     async: true,
     crossDomain: true,
-    url: 'https://realty-mole-property-api.p.rapidapi.com/saleListings?city=' + Qcity,
+    url:
+      "https://realty-mole-property-api.p.rapidapi.com/saleListings?city=" +
+      Qcity,
     data: {
       state: Qstate,
       bedrooms: Bed_rooms,
-      bathrooms: bath_rooms
+      bathrooms: bath_rooms,
     },
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '1fc0b1137emsha592badbb2ae9fap1a1cbajsn46535d340b45',
-      'X-RapidAPI-Host': 'realty-mole-property-api.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "1fc0b1137emsha592badbb2ae9fap1a1cbajsn46535d340b45",
+      "X-RapidAPI-Host": "realty-mole-property-api.p.rapidapi.com",
+    },
   };
 
   $.ajax(settings).done(function (response) {
     console.log(response);
+    localStorage.setItem("houseSearch", JSON.stringify(response));
   });
-  }
-getHousing();
+}
+
+
+//search for jobs
+function getJobs(searchCity, searchKeywords) {
+  //Variable to put into param
+  var url = "https://jooble.org/api/";
+  var key = "66db19d3-8777-40aa-a3c1-d214c3c4b8ea";
+  var params = "{ keywords: "+ searchKeywords +","+ "location: " + searchCity+ "}";
+  //------------------------------
+
+  //create xmlHttpRequest object
+  var http = new XMLHttpRequest();
+  //open connection. true - asynchronous, false - synchronous
+  http.open("POST", url + key, true);
+
+  //Send the proper header information
+  http.setRequestHeader("Content-type", "application/json");
+
+  //Callback when the state changes
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+      alert(http.responseText);
+      localStorage.setItem("JobSearch", http.responseText);
+    }
+  };
+  //Send request to the server
+  http.send(params);
+}
+
+function filterData() {
+  var jobInfo = localStorage.getItem("JobSearch");
+  var houseInfo = localStorage.getItem("houseSearch");
+
+  console.log(JSON.parse(jobInfo));
+  console.log(JSON.parse(houseInfo));
+}
+
+
+
+//render single card to page
+var container = $("#card-container");
+
+//add more params tommorow eg. Position, salary, description, link, 
+function renderCard(JobTitle, HouseLink) {
+  var newCard = $("<div>");
+
+  newCard.attr("class", "card");
+
+  var face1 = $("<div>").attr("class", "face face1").appendTo(newCard);
+  var faceContent1 = $("<div>").attr("class", "content").appendTo(face1);
+  $("<h3>").text(JobTitle).appendTo(faceContent1);
+
+  var face2 = $("<div>").attr("class", "face face2").appendTo(newCard);
+  var faceContent2 = $("<div>").attr("class", "content").appendTo(face2);
+  $("<p>").text(HouseLink).appendTo(faceContent2);
+
+  newCard.appendTo(container);
+}
+
+
