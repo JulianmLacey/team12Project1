57bafa475eb0a017219373d18a7a1d06fa3bddc6
Jay Liu <jayliu89@gmail.com>
2023-07-19 17:22:08 -0700

    updated readme file. links, gifs and images added

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
-housing {
-price: $$$$,
-bedrooms: 2,
-bathroom: 2,
-Property Type : "Condo", "Home" etc etc,
-squareFootage: 1111,
-yearBuilt** : 2023
+# Best Place? # 
+***Looking for a place? Why not get the best place?***
 
-}
+## User Story ##
+
+As a User, I want to see a navbar on the website
+WHEN I search for a job title in a city, and a enter a home with a certain number of bedrooms and bathrooms,
+THEN I will see cards populated on the screen with all the relavent information.
+
+## User Guide ##
+Once user opens the app, they will be presented with a home page and navbar. The navbar will have placeholder text to guide the user for what value they should search.
+
+![Home Page](/assets/images/Homepage.png)<br>
+**Navbar with placeholder text**
+
+Once on the home page, the user will be enter a job title that they are interested in applying for.
+
+![Job Search](</assets/images/Best Place Job Search.gif>)<br>
+**Users enter "Software Engineer" for their job search**
+
+Once the user has selected a city and filled out a number of bedrooms and bathrooms they wish their future home to have, they will be presented with a card.!
+
+![Best Place Full Demo](</assets/images/Best Place Card Demo.gif>)<br>
+**User has entered all parameters for search query and app will populate a card to display info**
+
+## Technologies Used ##
+
+HTML      
+CSS<br>
+JavaScript<br>
+[Jooble API](https://jooble.org/api/about)<br>
+[Find Places to Live API](https://rapidapi.com/apimaker/api/find-places-to-live) <br>
+[Realty Mole API](https://rapidapi.com/realtymole/api/realty-mole-property-api)<br>
+[Jquery+Ajax](https://api.jquery.com/jquery.ajax/)<br>
+[Blaze UI](https://www.blazeui.com/) <br>
+[nodeJS](https://nodejs.org/en)<br>
+[Heroku](https://www.heroku.com/)<br>
+[expressJS](https://expressjs.com/)<br>
+
+## Links ##
+
+[Github](https://github.com/JulianmLacey/team12Project1/tree/Julian/working)<br>
+[Deployed Site](https://shielded-sea-18665-6a243915e16d.herokuapp.com/)
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -62,49 +62,154 @@
 
 // renderCard()
 
+var JobElementInput = $("#job_input");
+var cityElementInput = $("#city_input");
+var bathroomElementInput = $("#bathroom_input");
+var bedRoomElementInput = $("#bedroom_input");
 
-$("#searchButton").on("click", renderCard)
 
-var container = $("#card-container");
-function renderCard(){
 
-var newCard = $("<div>");
 
-newCard.attr("class", "card");
 
-  var face1 = $("<div>").attr("class", "face face1").appendTo(newCard);
-  var faceContent1 = $("<div>").attr("class", "content").appendTo(face1);
-  $("<h3>").text("JOB TITLE").appendTo(faceContent1);
-  $("<h3>").text("Salary: ").appendTo(faceContent1);
+$("#searchButton").on("click", runSearch);
 
-  var face2 = $("<div>").attr("class", "face face2").appendTo(newCard);
-  var faceContent2 = $("<div>").attr("class", "content").appendTo(face2);
-  $("<h3>").text("House Stuff").appendTo(faceContent2);
-  $("<p>").text("housePrice").appendTo(faceContent2);
-  $("<p>").text("bedRooms").appendTo(faceContent2);
-  $("<p>").text("propertyType").appendTo(faceContent2);
-  $("<p>").text("squareFootage").appendTo(faceContent2);
+//search entry point
+function runSearch() {
+    var JobElementInputValue = JobElementInput.val();
+    var cityElementInputValue = cityElementInput.val();
+    var bathroomElementInputValue = bathroomElementInput.val();
+    var bedRoomElementInputValue = bedRoomElementInput.val();
+
+
+
+  console.log("searching");
   
-  // up to you if you think it's important
-  $("<p>").text("yearBuilt").attr("class", "last").appendTo(faceContent2);
 
-  var divider = $("<div>")
-  
-  divider.attr("class", "divider").appendTo(face2)
-  $("<h3>").text("City Report Card").appendTo(divider);
-  $("<p>").text("Overall Niche Grade").appendTo(divider)
-  $("<p>").text("Public Schools").appendTo(divider)
-  $("<p>").text("Crime & Safety").appendTo(divider)
-  $("<p>").text("Nightlife").appendTo(divider)
-  $("<p>").text("Good for Families").appendTo(divider)
-  $("<p>").text("Weather").appendTo(divider)
-  $("<p>").text("Cost of Living").appendTo(divider)
-  $("<p>").text("Health & Fitness").appendTo(divider)
+  //filterData();
+    console.log(JobElementInput.val());
+    console.log(cityElementInput.val());
+    console.log(bathroomElementInput.val());
+    console.log(bedRoomElementInput.val());
 
-  newCard.appendTo(container);
+
+    //check for valid input
+    if(JobElementInputValue == ""){
+        //alert bad jab input
+        return;
+    }
+    if(cityElementInputValue == ""){
+        //alert bad location input
+        return;
+    }
+    if(bathroomElementInputValue == ""){
+        
+    }
+    if(bedRoomElementInputValue == ""){
+        
+    }
+
+    //get the job input to be replaced with filterData(); but just saved to local storage to save calls
+    getJobs(cityElementInput.val(), JobElementInput.val());
+    var jobInfo = JSON.parse(localStorage.getItem("JobSearch"));
+
+
+    //getHousing(cityElementInput.val(), bathroomElementInput.val(), bedRoomElementInput.val());
+    var houseInfo = JSON.parse(localStorage.getItem("houseSearch"));
+
+    var pagelength = (jobInfo.totalCount>houseInfo.length)?houseInfo.length:jobInfo.totalCount;
+
+    console.log(pagelength);
+    for(var i = 0; i<pagelength;i++){
+        renderCard(jobInfo.jobs[i].company, houseInfo[i].formattedAddress);
+    }
+
 
 }
 
+
+//Temp variables for getHousing Function...to be removed later*
+
+
+//may need to add state param*
+function getHousing(Qcity, Bed_rooms, bath_rooms) {
+
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url:
+      "https://realty-mole-property-api.p.rapidapi.com/saleListings?city=" +
+      Qcity,
+    data: {
+      
+      bedrooms: Bed_rooms,
+      bathrooms: bath_rooms,
+    },
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "1fc0b1137emsha592badbb2ae9fap1a1cbajsn46535d340b45",
+      "X-RapidAPI-Host": "realty-mole-property-api.p.rapidapi.com",
+    },
+  };
+    // Api request, .done - on successful callback
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    localStorage.setItem("houseSearch", JSON.stringify(response));
+  });
+}
+
+
+
+function getJobs(searchCity, searchKeywords) {
+ 
+  var url = "https://jooble.org/api/";
+  var key = "66db19d3-8777-40aa-a3c1-d214c3c4b8ea";
+  var params = "{ keywords: "+ searchKeywords +","+ "location: " + searchCity+ "}";
+
+  var http = new XMLHttpRequest();
+ 
+  http.open("POST", url + key, true);
+
+  http.setRequestHeader("Content-type", "application/json");
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+      localStorage.setItem("JobSearch", http.responseText);
+    }
+  };
+  http.send(params);
+}
+
+function filterData() {
+  var jobInfo = localStorage.getItem("JobSearch");
+  var houseInfo = localStorage.getItem("houseSearch");
+
+  console.log(JSON.parse(jobInfo));
+  console.log(JSON.parse(houseInfo));
+}
+
+
+
+//render single card to page
+var container = $("#card-container");
+
+//add more params tommorow eg. Position, salary, description, link
+//house price, address, property type,, square footage, year built, description
+function renderCard(JobTitle, HouseLink) {
+  var newCard = $("<div>");
+
+  newCard.attr("class", "card");
+
+    //top card - jobs
+  var face1 = $("<div>").attr("class", "face face1").appendTo(newCard);
+  var faceContent1 = $("<div>").attr("class", "content").appendTo(face1);
+  $("<h3>").text(JobTitle).appendTo(faceContent1);
+
+    //under card
+  var face2 = $("<div>").attr("class", "face face2").appendTo(newCard);
+  var faceContent2 = $("<div>").attr("class", "content").appendTo(face2);
+  $("<p>").text(HouseLink).appendTo(faceContent2);
+
+  newCard.appendTo(container);
+}
 
 //-----------------Get Housing Options-------------------
 // var Qcity = 'Austin', Qstate = 'TX';
@@ -132,3 +237,4 @@ newCard.attr("class", "card");
 //   });
 //   }
 // getHousing();
+
